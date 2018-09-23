db1615b908519009e068f6ca248048d0c1b053ec
Y. Mayer <whythecode@gmail.com>
2018-09-23 21:59:07 +0100

    add dom exercises

---
+++ dom/exercise.js
@@ -0,0 +1,31 @@
+// DOM
+
+/**
+ * 1. Add 2 other list items to the list with the ID 'list-with-id'. For example, with the content 'F' and 'G'.
+ */
+
+
+/**
+ * 2. Then, delete the first two ('A' and 'B') items of the list.
+ */
+
+
+/**
+ * 3. Add an event listener to each list item, so that when clicked, the word "click" is added to the text.
+ */
+
+
+/**
+ * 4. Now, when clicking on each list item, also trigger an event called 'custom:my-click' on that same element.
+ */
+
+
+/**
+ * 5. Listen for the 'custom:my-click' at the 'list-with-id'. Whenever it's fired, add another item to the list.
+ */
+
+
+/**
+ * 6. Improve the functionality from exercises 3 and 4, so that the 'click' event listener is only added once instead
+ * of to every list item individually.
+ */
