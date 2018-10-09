61b8a058b28e3ef56638f5c6202b7217a266fa46
Sebastian Springer <info@sebastian-springer.com>
2018-10-09 09:58:06 +0200

    doc

--- workshop/01-index.js
+++ workshop/01-index.js
@@ -13,3 +13,52 @@ console.log('https://kangax.github.io/compat-table/es6/');
 
 // To debug a file:
 console.log('node --inspect-brk 01-index.js');
+
+var a = '1';
+a = false;
+a = 1;
+a = 1.1;
+true;
+false;
+var b;
+console.log(b);
+b = null;
+
+1 / 'aaa';
+
+1 / 0;
+-1 / 0;
+
+var arr = [];
+arr.push(1);
+console.log(arr[0]);
+
+var obj = {};
+obj.a = 1;
+obj.idjsaiodjas = 3;
+obj['jidsaodas-aisjdisa-32432'] = 3;
+obj.jidsajidaAdsiajdiasjAdjisajdia = 4;
+
+Date;
+Math;
+String;
+Array;
+Date.now();
+Math.PI;
+Math.max(0, 10);
+Math.floor(1.5);
+
+var str = '"einfach"';
+var zweifach = "'zweifach'";
+var template = `ho
+ho
+${str}
+${1 + 2}
+${Math.max(0, 10)}
+ho
+ho`;
+
+console.log('https://developer.mozilla.org/de/docs/Web/JavaScript/Reference/template_strings');
+
+
+
