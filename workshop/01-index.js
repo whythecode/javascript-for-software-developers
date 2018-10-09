a470e87c3c6fd05a938ab6ef219b80bdffb2bfb0
Sebastian Springer <info@sebastian-springer.com>
2018-10-09 09:18:27 +0200

    doc

---
+++ workshop/01-index.js
@@ -0,0 +1,15 @@
+console.log('Willkommen zu: JavaScript für (richtige) Softwareentwickler');
+console.log('für Einsteiger und Umsteiger');
+
+console.log('Yara Mayer, Evia, @whythecode');
+
+console.log('Sebastian Springer, MaibornWolff, @basti_springer');
+
+console.log('Der JavaScript Standard: https://www.ecma-international.org/publications/files/ECMA-ST/Ecma-262.pdf');
+
+console.log('https://developer.mozilla.org/de/');
+
+console.log('https://kangax.github.io/compat-table/es6/');
+
+// To debug a file:
+console.log('node --inspect-brk 01-index.js');
